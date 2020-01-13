969d468a65e45f7b2d8b2a0175e43eca66794af0
Riccardo Persiani <r.persiani92@gmail.com>
2020-01-13 17:33:16 +0000

    feat(block): add litecoin validate pow and relative test

--- src/blockdata/block.rs
+++ src/blockdata/block.rs
@@ -22,15 +22,16 @@
 
 use hashes::{sha256d, Hash};
 
-use util;
-use util::Error::{BlockBadTarget, BlockBadProofOfWork};
-use util::hash::{BitcoinHash, MerkleRoot, bitcoin_merkle_root};
-use util::uint::Uint256;
-use consensus::encode::Encodable;
-use network::constants::Network;
-use blockdata::transaction::Transaction;
 use blockdata::constants::max_target;
+use blockdata::transaction::Transaction;
+use consensus::encode::Encodable;
 use hashes::HashEngine;
+use network::constants::Network;
+use scrypt::{scrypt, ScryptParams};
+use util;
+use util::hash::{bitcoin_merkle_root, BitcoinHash, MerkleRoot};
+use util::uint::Uint256;
+use util::Error::{BlockBadProofOfWork, BlockBadTarget};
 
 /// A block header, which contains all the block's information except
 /// the actual transactions
@@ -58,43 +59,52 @@ pub struct Block {
     /// The block header
     pub header: BlockHeader,
     /// List of transactions contained in the block
-    pub txdata: Vec<Transaction>
+    pub txdata: Vec<Transaction>,
 }
 
 impl Block {
     /// Create a new block from constituent parts
     pub fn new(header: BlockHeader, txdata: Vec<Transaction>) -> Self {
-        Block {
-            header,
-            txdata
-        }
+        Block { header, txdata }
     }
 
     /// check if merkle root of header matches merkle root of the transaction list
-    pub fn check_merkle_root (&self) -> bool {
+    pub fn check_merkle_root(&self) -> bool {
         self.header.merkle_root == self.merkle_root()
     }
 
     /// check if witness commitment in coinbase is matching the transaction list
     pub fn check_witness_commitment(&self) -> bool {
-
         // witness commitment is optional if there are no transactions using SegWit in the block
-        if self.txdata.iter().all(|t| t.input.iter().all(|i| i.witness.is_empty())) {
+        if self
+            .txdata
+            .iter()
+            .all(|t| t.input.iter().all(|i| i.witness.is_empty()))
+        {
             return true;
         }
         if self.txdata.len() > 0 {
             let coinbase = &self.txdata[0];
             if coinbase.is_coin_base() {
                 // commitment is in the last output that starts with below magic
-                if let Some(pos) = coinbase.output.iter()
-                    .rposition(|o| {
-                        o.script_pubkey.len () >= 38 &&
-                        o.script_pubkey[0..6] == [0x6a, 0x24, 0xaa, 0x21, 0xa9, 0xed] }) {
-                    let commitment = sha256d::Hash::from_slice(&coinbase.output[pos].script_pubkey.as_bytes()[6..38]).unwrap();
+                if let Some(pos) = coinbase.output.iter().rposition(|o| {
+                    o.script_pubkey.len() >= 38
+                        && o.script_pubkey[0..6] == [0x6a, 0x24, 0xaa, 0x21, 0xa9, 0xed]
+                }) {
+                    let commitment = sha256d::Hash::from_slice(
+                        &coinbase.output[pos].script_pubkey.as_bytes()[6..38],
+                    )
+                    .unwrap();
                     // witness reserved value is in coinbase input witness
-                    if coinbase.input[0].witness.len() == 1 && coinbase.input[0].witness[0].len() == 32 {
+                    if coinbase.input[0].witness.len() == 1
+                        && coinbase.input[0].witness[0].len() == 32
+                    {
                         let witness_root = self.witness_root();
-                        return commitment == Self::compute_witness_commitment(&witness_root, coinbase.input[0].witness[0].as_slice())
+                        return commitment
+                            == Self::compute_witness_commitment(
+                                &witness_root,
+                                coinbase.input[0].witness[0].as_slice(),
+                            );
                     }
                 }
             }
@@ -103,7 +113,10 @@ impl Block {
     }
 
     /// compute witness commitment for the transaction list
-    pub fn compute_witness_commitment (witness_root: &sha256d::Hash, witness_reserved_value: &[u8]) -> sha256d::Hash {
+    pub fn compute_witness_commitment(
+        witness_root: &sha256d::Hash,
+        witness_reserved_value: &[u8],
+    ) -> sha256d::Hash {
         let mut encoder = sha256d::Hash::engine();
         witness_root.consensus_encode(&mut encoder).unwrap();
         encoder.input(witness_reserved_value);
@@ -112,8 +125,8 @@ impl Block {
 
     /// Merkle root of transactions hashed for witness
     pub fn witness_root(&self) -> sha256d::Hash {
-        let mut txhashes = vec!(sha256d::Hash::default());
-        txhashes.extend(self.txdata.iter().skip(1).map(|t|t.bitcoin_hash()));
+        let mut txhashes = vec![sha256d::Hash::default()];
+        txhashes.extend(self.txdata.iter().skip(1).map(|t| t.bitcoin_hash()));
         bitcoin_merkle_root(txhashes)
     }
 }
@@ -153,7 +166,10 @@ impl BlockHeader {
         let (mant, expt) = {
             let unshifted_expt = self.bits >> 24;
             if unshifted_expt <= 3 {
-                ((self.bits & 0xFFFFFF) >> (8 * (3 - unshifted_expt as usize)), 0)
+                (
+                    (self.bits & 0xFFFFFF) >> (8 * (3 - unshifted_expt as usize)),
+                    0,
+                )
             } else {
                 (self.bits & 0xFFFFFF, 8 * ((self.bits >> 24) - 3))
             }
@@ -193,16 +209,32 @@ impl BlockHeader {
     /// Checks that the proof-of-work for the block is valid.
     pub fn validate_pow(&self, required_target: &Uint256) -> Result<(), util::Error> {
         use byteorder::{ByteOrder, LittleEndian};
-
+        use consensus::encode::serialize;
         let target = &self.target();
         if target != required_target {
             return Err(BlockBadTarget);
         }
-        let data: [u8; 32] = self.bitcoin_hash().into_inner();
-        let mut ret = [0u64; 4];
-        LittleEndian::read_u64_into(&data, &mut ret);
-        let hash = &Uint256(ret);
-        if hash <= target { Ok(()) } else { Err(BlockBadProofOfWork) }
+        let block_header_vector = &serialize(self);
+        let params = ScryptParams::new(10, 1, 1).unwrap();
+        let mut data = [0u8; 32];
+        match scrypt(
+            &block_header_vector,
+            &block_header_vector,
+            &params,
+            &mut data,
+        ) {
+            Ok(_) => {
+                let mut ret = [0u64; 4];
+                LittleEndian::read_u64_into(&data, &mut ret);
+                let hash = &Uint256(ret);
+                if hash <= target {
+                    Ok(())
+                } else {
+                    Err(BlockBadProofOfWork)
+                }
+            }
+            Err(_) => Err(BlockBadProofOfWork),
+        }
     }
 
     /// Returns the total work of the block
@@ -220,6 +252,7 @@ impl BlockHeader {
 impl BitcoinHash for BlockHeader {
     fn bitcoin_hash(&self) -> sha256d::Hash {
         use consensus::encode::serialize;
+        // serialize encode and object into a vector
         sha256d::Hash::hash(&serialize(self))
     }
 }
@@ -230,44 +263,76 @@ impl BitcoinHash for Block {
     }
 }
 
-impl_consensus_encoding!(BlockHeader, version, prev_blockhash, merkle_root, time, bits, nonce);
+impl_consensus_encoding!(
+    BlockHeader,
+    version,
+    prev_blockhash,
+    merkle_root,
+    time,
+    bits,
+    nonce
+);
 impl_consensus_encoding!(Block, header, txdata);
-serde_struct_impl!(BlockHeader, version, prev_blockhash, merkle_root, time, bits, nonce);
+serde_struct_impl!(
+    BlockHeader,
+    version,
+    prev_blockhash,
+    merkle_root,
+    time,
+    bits,
+    nonce
+);
 serde_struct_impl!(Block, header, txdata);
 
 #[cfg(test)]
 mod tests {
     use super::*;
-    use std::str::FromStr;
-    use util::hash::MerkleRoot;
-    use hex::decode as hex_decode;
     use blockdata::block::{Block, BlockHeader};
     use consensus::encode::{deserialize, serialize};
+    use hex::decode as hex_decode;
+    use std::str::FromStr;
+    use util::hash::MerkleRoot;
+
+    #[test]
+    fn test_scrypt() {
+        // https://chainz.cryptoid.info/ltc/block.dws?1770654.htm
+        let time: u32 = 1578931097;
+        let bits: u32 = 436403550;
+        let nonce: u32 = 2456562066;
+        let version: u32 = 536870912;
+        let merkle_root = sha256d::Hash::from_str(
+            "3b776a5cc5e78bba1356414ea6b145e94c45d9ec2274a30acd8e89a28b392dc2",
+        )
+        .unwrap();
+        let prev_blockhash = sha256d::Hash::from_str(
+            "3dadd3dd482899a545b7e0548016db256e3acdb7bae1831e94b710d17a06e15d",
+        )
+        .unwrap();
+        let litecoin_header =
+            BlockHeader::new(time, bits, nonce, version, merkle_root, prev_blockhash);
+        litecoin_header.validate_pow(&litecoin_header.target());
+    }
 
     #[test]
     fn should_create_new_block_header_from_constituent_parts() {
         // NOTE: Test vector == bitcoing mainnet block #605083
         let expected_bitcoin_hash = sha256d::Hash::from_str(
-            "000000000000000000071b0604c7e29d51b1e8c64d22fb53f21ddb0f685a5c31"
-        ).unwrap();
+            "000000000000000000071b0604c7e29d51b1e8c64d22fb53f21ddb0f685a5c31",
+        )
+        .unwrap();
         let time: u32 = 1574518961;
         let bits: u32 = 387297854;
         let nonce: u32 = 206815602;
         let version: u32 = 536870912;
         let merkle_root = sha256d::Hash::from_str(
-            "7d0d017c486f8fbab8d9d1f9e1104856087b84cef2bfa49df1afa11d07ca6e8c"
-        ).unwrap();
+            "7d0d017c486f8fbab8d9d1f9e1104856087b84cef2bfa49df1afa11d07ca6e8c",
+        )
+        .unwrap();
         let prev_blockhash = sha256d::Hash::from_str(
-            "0000000000000000000c814d010966506828dc4946fbcb7da9b8491b5a262fd9"
-        ).unwrap();
-        let block = BlockHeader::new(
-            time,
-            bits,
-            nonce,
-            version,
-            merkle_root,
-            prev_blockhash,
-        );
+            "0000000000000000000c814d010966506828dc4946fbcb7da9b8491b5a262fd9",
+        )
+        .unwrap();
+        let block = BlockHeader::new(time, bits, nonce, version, merkle_root, prev_blockhash);
         let bitcoin_hash = block.bitcoin_hash();
         assert!(block.time == time);
         assert!(block.bits == bits);
@@ -286,8 +351,10 @@ mod tests {
         let some_block = hex_decode("010000004ddccd549d28f385ab457e98d1b11ce80bfea2c5ab93015ade4973e400000000bf4473e53794beae34e64fccc471dace6ae544180816f89591894e0f417a914cd74d6e49ffff001d323b3a7b0201000000010000000000000000000000000000000000000000000000000000000000000000ffffffff0804ffff001d026e04ffffffff0100f2052a0100000043410446ef0102d1ec5240f0d061a4246c1bdef63fc3dbab7733052fbbf0ecd8f41fc26bf049ebb4f9527f374280259e7cfa99c48b0e3f39c51347a19a5819651503a5ac00000000010000000321f75f3139a013f50f315b23b0c9a2b6eac31e2bec98e5891c924664889942260000000049483045022100cb2c6b346a978ab8c61b18b5e9397755cbd17d6eb2fe0083ef32e067fa6c785a02206ce44e613f31d9a6b0517e46f3db1576e9812cc98d159bfdaf759a5014081b5c01ffffffff79cda0945903627c3da1f85fc95d0b8ee3e76ae0cfdc9a65d09744b1f8fc85430000000049483045022047957cdd957cfd0becd642f6b84d82f49b6cb4c51a91f49246908af7c3cfdf4a022100e96b46621f1bffcf5ea5982f88cef651e9354f5791602369bf5a82a6cd61a62501fffffffffe09f5fe3ffbf5ee97a54eb5e5069e9da6b4856ee86fc52938c2f979b0f38e82000000004847304402204165be9a4cbab8049e1af9723b96199bfd3e85f44c6b4c0177e3962686b26073022028f638da23fc003760861ad481ead4099312c60030d4cb57820ce4d33812a5ce01ffffffff01009d966b01000000434104ea1feff861b51fe3f5f8a3b12d0f4712db80e919548a80839fc47c6a21e66d957e9c5d8cd108c7a2d2324bad71f9904ac0ae7336507d785b17a2c115e427a32fac00000000").unwrap();
         let cutoff_block = hex_decode("010000004ddccd549d28f385ab457e98d1b11ce80bfea2c5ab93015ade4973e400000000bf4473e53794beae34e64fccc471dace6ae544180816f89591894e0f417a914cd74d6e49ffff001d323b3a7b0201000000010000000000000000000000000000000000000000000000000000000000000000ffffffff0804ffff001d026e04ffffffff0100f2052a0100000043410446ef0102d1ec5240f0d061a4246c1bdef63fc3dbab7733052fbbf0ecd8f41fc26bf049ebb4f9527f374280259e7cfa99c48b0e3f39c51347a19a5819651503a5ac00000000010000000321f75f3139a013f50f315b23b0c9a2b6eac31e2bec98e5891c924664889942260000000049483045022100cb2c6b346a978ab8c61b18b5e9397755cbd17d6eb2fe0083ef32e067fa6c785a02206ce44e613f31d9a6b0517e46f3db1576e9812cc98d159bfdaf759a5014081b5c01ffffffff79cda0945903627c3da1f85fc95d0b8ee3e76ae0cfdc9a65d09744b1f8fc85430000000049483045022047957cdd957cfd0becd642f6b84d82f49b6cb4c51a91f49246908af7c3cfdf4a022100e96b46621f1bffcf5ea5982f88cef651e9354f5791602369bf5a82a6cd61a62501fffffffffe09f5fe3ffbf5ee97a54eb5e5069e9da6b4856ee86fc52938c2f979b0f38e82000000004847304402204165be9a4cbab8049e1af9723b96199bfd3e85f44c6b4c0177e3962686b26073022028f638da23fc003760861ad481ead4099312c60030d4cb57820ce4d33812a5ce01ffffffff01009d966b01000000434104ea1feff861b51fe3f5f8a3b12d0f4712db80e919548a80839fc47c6a21e66d957e9c5d8cd108c7a2d2324bad71f9904ac0ae7336507d785b17a2c115e427a32fac").unwrap();
 
-        let prevhash = hex_decode("4ddccd549d28f385ab457e98d1b11ce80bfea2c5ab93015ade4973e400000000").unwrap();
-        let merkle = hex_decode("bf4473e53794beae34e64fccc471dace6ae544180816f89591894e0f417a914c").unwrap();
+        let prevhash =
+            hex_decode("4ddccd549d28f385ab457e98d1b11ce80bfea2c5ab93015ade4973e400000000").unwrap();
+        let merkle =
+            hex_decode("bf4473e53794beae34e64fccc471dace6ae544180816f89591894e0f417a914c").unwrap();
 
         let decode: Result<Block, _> = deserialize(&some_block);
         let bad_decode: Result<Block, _> = deserialize(&cutoff_block);
@@ -317,12 +384,14 @@ mod tests {
 
         let decode: Result<Block, _> = deserialize(&segwit_block);
 
-        let prevhash = hex_decode("2aa2f2ca794ccbd40c16e2f3333f6b8b683f9e7179b2c4d74906000000000000").unwrap();
-        let merkle = hex_decode("10bc26e70a2f672ad420a6153dd0c28b40a6002c55531bfc99bf8994a8e8f67e").unwrap();
+        let prevhash =
+            hex_decode("2aa2f2ca794ccbd40c16e2f3333f6b8b683f9e7179b2c4d74906000000000000").unwrap();
+        let merkle =
+            hex_decode("10bc26e70a2f672ad420a6153dd0c28b40a6002c55531bfc99bf8994a8e8f67e").unwrap();
 
         assert!(decode.is_ok());
         let real_decode = decode.unwrap();
-        assert_eq!(real_decode.header.version, 0x20000000);  // VERSIONBITS but no bits set
+        assert_eq!(real_decode.header.version, 0x20000000); // VERSIONBITS but no bits set
         assert_eq!(serialize(&real_decode.header.prev_blockhash), prevhash);
         assert_eq!(serialize(&real_decode.header.merkle_root), merkle);
         assert_eq!(real_decode.header.merkle_root, real_decode.merkle_root());
@@ -340,9 +409,12 @@ mod tests {
     fn compact_roundrtip_test() {
         let some_header = hex_decode("010000004ddccd549d28f385ab457e98d1b11ce80bfea2c5ab93015ade4973e400000000bf4473e53794beae34e64fccc471dace6ae544180816f89591894e0f417a914cd74d6e49ffff001d323b3a7b").unwrap();
 
-        let header: BlockHeader = deserialize(&some_header).expect("Can't deserialize correct block header");
+        let header: BlockHeader =
+            deserialize(&some_header).expect("Can't deserialize correct block header");
 
-        assert_eq!(header.bits, BlockHeader::compact_target_from_u256(&header.target()));
+        assert_eq!(
+            header.bits,
+            BlockHeader::compact_target_from_u256(&header.target())
+        );
     }
 }
-
